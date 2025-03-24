3dbc3c8062defc4b9dd53adc2fa222d8ae88edb3
Daniel Cregg <danielcregg@users.noreply.github.com>
2025-03-24 13:34:37 +0000

    Implement movie serialization and deserialization functionality

--- src/ie/atu/serialization/Main.java
+++ src/ie/atu/serialization/Main.java
@@ -1,7 +1,30 @@
 package ie.atu.serialization;
 
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Create a movie object
+        Movie movie = new Movie("The Matrix", "Wachowskis", 1999, 8.7);
+        System.out.println("Original Movie: " + movie);
+        
+        // Serialize
+        try (FileOutputStream fileOut = new FileOutputStream("resources/movie.ser");
+             ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
+            out.writeObject(movie);
+            System.out.println("Movie has been serialized");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        // Deserialize
+        try (FileInputStream fileIn = new FileInputStream("resources/movie.ser");
+             ObjectInputStream in = new ObjectInputStream(fileIn)) {
+            Movie deserializedMovie = (Movie) in.readObject();
+            System.out.println("Movie has been deserialized");
+            System.out.println("Deserialized Movie: " + deserializedMovie);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
